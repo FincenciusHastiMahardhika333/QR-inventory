0f3f93e4a602ed2919881430ff8fd1cb551981e8
MuizZer07 <muizxzer07@gmail.com>
2019-06-14 10:08:50 +0600

    a few fixes

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^item/show/(\d+)', views.show_item , name="show_item"),
     url(r'^item/search_results', views.search_results , name="search_results"),
     url(r'^item/search', views.search_item_Form.as_view() , name="search_item"),
+    url(r'^item/delete/(\d+)', views.delete_item , name="delete_item"),
+    url(r'^item/edit/(?P<pk>\d+)', views.ItemUpdate.as_view() , name="edit_item"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -11,18 +11,23 @@ from PIL import Image
 from  django.core.files import File
 from QRinventory import settings
 import pyzbar.pyzbar as pyzbar
+import random, string
+from django.contrib import messages
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
 
 def get_qrcode(tag):
     qr = pyqrcode.create(tag)
-    filename = tag + '.png'
+    a = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(7)) 
+    filename = a + '.png'
     qr.png('media/temp/' + filename, scale=7)
     
     return filename
 
 class ItemForm(forms.Form):
-    privacy_choices= [
-    ('Private', 'Private'),
-    ('Public', 'Public'),
+    privacy_choices = [
+        ('Private', 'Private'),
+        ('Public', 'Public'),
     ]
 
     name = forms.CharField(label='Item Name')
@@ -38,17 +43,22 @@ class Create_item_Form(FormView):
     def form_valid(self, form):
         try:
             newly_created_item = Item()
-            newly_created_item.owner = self.request.user
+            newly_created_item.owner = self.request.user    
             newly_created_item.name = form.cleaned_data['name']
             newly_created_item.type = form.cleaned_data['itemtype']
             newly_created_item.desc = form.cleaned_data['desc']
-            tag = form.cleaned_data['name'] + form.cleaned_data['itemtype']
+
+            a = ''.join(random.choice(string.ascii_uppercase) for _ in range(6))
+            b = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+            tag = b + form.cleaned_data['name'][:7] + a + '_@' + str(self.request.user.id) 
             filename = get_qrcode(tag)
             filepath = settings.MEDIA_ROOT + "/temp/" + filename
             newly_created_item.qrcode = filepath
             newly_created_item.tag = tag
             newly_created_item.privacy = form.cleaned_data['privacy']
             newly_created_item.save()
+
+            messages.add_message(self.request, messages.INFO, 'Item has been created!')
             return HttpResponseRedirect('/profile')
         except IntegrityError as e:
             return redirect('/profile')
@@ -68,7 +78,6 @@ class search_item_Form(FormView):
             
             text = pyzbar.decode(Image.open(form.cleaned_data['qrcode']))
             text = text[0].data.decode("utf-8")
-           
             searching = form.cleaned_data['searching']
             if searching:
                 items = Item.objects.filter(tag=text, privacy='Public')
@@ -85,3 +94,25 @@ def search_results(request):
 def show_item(request, item_id):
     item = Item.objects.get(id=item_id)
     return render(request, 'item/show_item.html', {'item': item})
+
+@login_required
+def delete_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+
+    if item.owner != request.user:
+        messages.add_message(request, messages.INFO, 'You can not delete the item!')
+        return redirect('/profile')
+    else:
+        item.delete()
+
+        messages.add_message(request, messages.INFO, 'Item has been deleted!')
+        return redirect('/profile')
+
+class ItemUpdate(UpdateView):
+    model = Item
+    fields = ['name', 'type', 'desc', 'privacy']
+    template_name = "item/item_update_form.html"
+
+    def get_success_url(self):
+        messages.add_message(self.request, messages.INFO, 'Item has been edited!')
+        return reverse_lazy('profile')
